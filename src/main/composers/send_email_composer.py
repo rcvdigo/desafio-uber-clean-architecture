50b97b95ef11dead64fa7c9fec7ce28f5b022231
rcvdigo <rcvdigo@gmail.com>
2024-02-26 04:17:24 -0300

    feat: Adicionando composer que faz a unian de todas as classes

--- src/main/composers/send_email_composer.py
+++ src/main/composers/send_email_composer.py
@@ -3,12 +3,12 @@ import os
 
 from src.main.infra.ses.ses_email_sender import SesEmailSender
 from src.main.application.email_sender_service import EmailSenderService
-from src.main.controllers.sender_controller import SenderController
+from src.main.controllers.email_sender_controller import SenderController
 
 
 def send_email_composer():
     email_sender_gateway = SesEmailSender(
-        service_name=os.getenv('NOME_DO_SERVICO'),
+        service_name=os.getenv('SES'),
         region_name=os.getenv('REGIAO'),
         access_key=os.getenv('CHAVE_ACESSO'),
         secret_key=os.getenv('CHAVE_SECRETA')
