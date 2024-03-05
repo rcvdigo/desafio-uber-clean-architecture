c90fa2501560fa1548aae2d911783a38ced0d8df
rcvdigo <rcvdigo@gmail.com>
2024-03-05 14:39:18 -0300

    feat: Adicionando a pasta static/, modularizando o js, css e imgs

--- src/main/server/server.py
+++ src/main/server/server.py
@@ -2,12 +2,16 @@ import os
 from flask import Flask
 from src.main.routes.http_routes import email_route_bp
 
-
+# Diretório do projeto
 path=os.path.dirname(__file__)
-# template_folder = os.path.join(path, '..', 'templates')
+
+# Diretório dos statics
+static_folder = os.path.abspath(os.path.join(path, '../../..', 'static'))
+
+# Diretório dos templates
 template_folder = os.path.abspath(os.path.join(path, '..', 'templates'))
 
-app = Flask(__name__, template_folder=template_folder)
+app = Flask(__name__, template_folder=template_folder, static_folder=static_folder)
 
 # Register Blueprints
 app.register_blueprint(blueprint=email_route_bp)
