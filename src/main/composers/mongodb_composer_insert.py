2e6bc55288c68f8e7f6a375073aff1831346b4bf
rcvdigo <rcvdigo@gmail.com>
2024-03-21 15:46:55 -0300

    fix: Correção do mongodb_composer_insert.py

--- src/main/composers/mongodb_composer_insert.py
+++ src/main/composers/mongodb_composer_insert.py
@@ -3,10 +3,11 @@ import os
 from pymongo import MongoClient
 from src.main.infra.repositories.mongo_db.mongo_db import MongoDb
 from src.main.application.database_service import DatabaseService
-from src.main.controllers.mongodb_controller_insert import MongodbControllerInsert
+from src.main.controllers.mongodb_controller_insert_html import MongodbControllerInsertHtml
+from src.main.controllers.mongodb_controller_insert_api import MongodbControllerInsertApi
 
 
-def mongodb_composer_insert():
+def mongodb_composer_insert_html():
     uri = os.getenv('MONGO_URI')
     database_mongodb_use_case = MongoDb(
         mongo_client=MongoClient(uri),
@@ -14,6 +15,19 @@ def mongodb_composer_insert():
         name_collection='desafio_dio'
     )
     database_mongodb_service = DatabaseService(use_case=database_mongodb_use_case)
-    controller = MongodbControllerInsert(use_case=database_mongodb_service)
+    controller = MongodbControllerInsertHtml(use_case=database_mongodb_service)
+    
+    return controller.handle
+
+
+def mongodb_composer_insert_api():
+    uri = os.getenv('MONGO_URI')
+    database_mongodb_use_case = MongoDb(
+        mongo_client=MongoClient(uri),
+        name_db='bancoMongoDB',
+        name_collection='desafio_dio'
+    )
+    database_mongodb_service = DatabaseService(use_case=database_mongodb_use_case)
+    controller = MongodbControllerInsertApi(use_case=database_mongodb_service)
     
     return controller.handle
